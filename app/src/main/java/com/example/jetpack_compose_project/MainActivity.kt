7d85f9a0bdf1b96fe089e1e6c372b11e3f296e97
VatanakChamroeun <ch.vatanak01@gmail.com>
2023-07-10 00:37:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jetpack_compose_project/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.jetpack_compose_project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpack_compose_project.ui.theme.Jetpack_compose_projectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Jetpack_compose_projectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Jetpack_compose_projectTheme {
+        Greeting("Android")
+    }
+}
